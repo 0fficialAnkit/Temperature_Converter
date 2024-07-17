8bd7889f46adb80f5e22b803ccf564345d5fbc40
Ankit Kumar <154526108+0fficialAnkit@users.noreply.github.com>
2024-07-17 12:36:37 +0530

    PasswordGenerator.cpp

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -1,18 +1,32 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
 using namespace std;
-int main() {
-    int length;
-    const char characters[]="!@#$%&*_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    int characterslength=sizeof(characters)-1;
-    srand(time(0));
-    cout<<"Enter the desired length for the password: ";
-    cin>>length;
-    cout<<"Generated Password: ";
-    for (int i=0;i<length; ++i) {
-        cout<<characters[rand()%characterslength];
+int main(){
+    int choice;
+    double temp, convertedTemp;
+    while (true){
+        cout<<"Temperature Conversion Program"<<endl;
+        cout<<"1. Convert from Celsius to Fahrenheit"<<endl;
+        cout<<"2. Convert from Fahrenheit to Celsius"<<endl;
+        cout<<"3. Exit" << endl;
+        cout<<"Enter your choice: ";
+        cin>>choice;
+        if (choice == 1) {
+            cout<<"Enter temperature in Celsius: ";
+            cin>>temp;
+            convertedTemp = (temp*9.0/5.0)+32;
+            cout<<temp<<" Celsius is "<< convertedTemp<<" Fahrenheit."<<endl;
+        } else if (choice == 2) {
+            cout<<"Enter temperature in Fahrenheit: ";
+            cin>>temp;
+            convertedTemp = (temp - 32)*5.0/9.0;
+            cout<<temp<<" Fahrenheit is "<<convertedTemp <<" Celsius."<<endl;
+        } else if (choice == 3) {
+            cout<<"Exiting program."<<endl;
+            break;
+        } else {
+            cout<<"Invalid choice."<<endl;
+        }
+        cout<<endl;
     }
-    cout<<endl;
     return 0;
 }
